be562110ba5c2afd02ca9f5de5b204cc78f8c35a
oliver-oloughlin <oliveroloughlin@hotmail.com>
2024-08-22 02:51:16 +0200

    enlarged color picker

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -3,7 +3,7 @@ import { DrawingCanvas } from "./DrawingCanvas";
 
 export function App() {
   return (
-    <main class="w-[100dvw] h-[100dvh] place-content-center grid">
+    <main class="w-[100dvw] h-[100dvh] place-content-center grid gap-2">
       <DrawingCanvas />
       <ColorPicker />
     </main>
--- client/src/app/ColorPicker.tsx
+++ client/src/app/ColorPicker.tsx
@@ -2,9 +2,10 @@ import { store, setColor } from "../utils/store";
 
 export function ColorPicker() {
   return (
-    <label class="flex gap-2 font-semibold">
+    <label class="flex gap-2 items-center font-semibold text-2xl">
       Color:
       <input
+        class="h-10 w-20"
         value={store.selectedColor}
         type="color"
         onInput={e => setColor(e.target.value)}
